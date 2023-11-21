3e6c0de7ffbbcb9a9d987b564037b26a5b4b3cae
InSik-j <150884733+InSik-j@users.noreply.github.com>
2023-11-21 17:30:45 +0900

    내파티_파티리스트 출력

---
+++ src/main/java/com/team1/careercanvas/Controller/PartyController.java
@@ -0,0 +1,38 @@
+package com.team1.careercanvas.Controller;
+
+
+import com.team1.careercanvas.mapper.PartyMapper;
+import com.team1.careercanvas.vo.PartyVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class PartyController {
+    private final PartyMapper mapper;
+
+    public PartyController(PartyMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    // 정인식 작업
+    @GetMapping("/myteam/main")
+    public ModelAndView myteamMain(HttpSession session){
+        ModelAndView mav = new ModelAndView();
+        String logId = (String) session.getAttribute("LogId");
+        String logStatus = (String) session.getAttribute("logStatus");
+        if (logStatus != "Y" && logId == null) {
+            mav.addObject("msg", "로그인 후 이용가능합니다.");
+            mav.setViewName("redirect:/");
+            //mav.setViewName("alert_page");
+            return mav;
+        }
+        List<PartyVO> vo = mapper.SelectPartyList(logId);
+        mav.addObject("pvo",vo);
+        mav.setViewName("myteam/myteam_main");
+        return mav;
+    }
+}
